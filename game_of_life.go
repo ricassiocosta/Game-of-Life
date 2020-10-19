7a8ebc8d37aff2e312c603c5cf44b62b44e1f2af
ricassiocosta <riksiocosta@gmail.com>
2020-10-19 12:06:47 -0300

    methods implemented still not working

--- game_of_life.go
+++ game_of_life.go
@@ -20,12 +20,39 @@ func (matrix *Matrix) IsAlive(x, y int) bool {
 
 //NextGen generate the Game of Life's next generation of cells
 func (matrix *Matrix) NextGen(x, y int) bool {
+	alive := 0
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			if dx == 0 && dy == 0 {
+				// don't count self
+				continue
+			}
+			if matrix.IsAlive(x+dx, y+dy) {
+				alive++
+			}
+		}
+	}
 	
+	if matrix.IsAlive(x, y) {
+		if alive > 3 || alive < 2 {
+			return false //dies
+		}
+
+		return true //keeping alive
+	} else if alive == 3 {
+		return true //revives
+	}
+
+	return false
 }
 
 //Populate the matrix 
 func (matrix *Matrix) Populate() {
-	
+	for y := 0; y < matrix.height; y++ {
+		for x := 0; x < matrix.width; x++ {
+			matrix.layer[x][y] = matrix.NextGen(x, y)
+		}
+	}
 }
 
 func (matrix *Matrix) String() string {
